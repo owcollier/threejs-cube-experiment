8c0ee6b7e2aabc55eaefe9c14913d6dcd5a4dd1a
Owcollier <owcollier@gmail.com>
2018-05-14 16:37:30 -0400

    added Polyhedron geometry, further tweaked mesh

--- js/index.js
+++ js/index.js
@@ -14,12 +14,26 @@ function init() {
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  material = new THREE.MeshNormalMaterial( { flatShading: true, morphTargets: true } );
+  let verticesOfCube = [
+    -1,-1,-1,    1,-1,-1,    1, 1,-1,    -1, 1,-1,
+    -1,-1, 1,    1,-1, 1,    1, 1, 1,    -1, 1, 1,
+  ];
+
+  let indicesOfFaces = [
+    2,1,0,    0,3,2,
+    0,4,7,    7,3,0,
+    0,1,5,    5,4,0,
+    1,2,6,    6,5,1,
+    2,3,7,    7,6,2,
+    4,5,6,    6,7,4
+  ];
+
+  geometry = new THREE.PolyhedronGeometry( verticesOfCube, indicesOfFaces, 6, 2 );
+  material = new THREE.MeshNormalMaterial( { flatShading: false , wireframe: true } );
   cube = new THREE.Mesh( geometry, material );
   scene.add( cube );
 
-  camera.position.z = 5;
+  camera.position.z = 15;
 
 
   controls = new THREE.OrbitControls( camera, renderer.domElement );
@@ -27,8 +41,8 @@ function init() {
   controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
   controls.dampingFactor = 0.25;
   controls.screenSpacePanning = true;
-  controls.minDistance = 2;
-  controls.maxDistance = 10;
+  controls.minDistance = 15;
+  controls.maxDistance = 100;
   controls.maxPolarAngle = Math.PI / 2;
 }
 
